010d172a5f524e850850bb0f1acd03df36452228
tsah20 <tanyasah20@gmail.com>
2017-05-29 15:07:09 +0530

    horray files are getting compared

---
+++ fileComparer/comparer.py
@@ -0,0 +1,9 @@
+import csv;
+with open('input1.csv', 'r') as t1, open('input2.csv', 'r') as t2:
+    fileone = t1.readlines()
+    filetwo = t2.readlines()
+
+with open('update.csv', 'w') as outFile:
+    for line in filetwo:
+        if line not in fileone:
+            outFile.write(line)
